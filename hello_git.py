880244e4d4b1eb868b94a1db59049a66c561791c
Eric Gumerov <egumerov@gmail.com>
2025-08-15 15:48:24 +0800

    Update hello_git py. Add for

--- hello_git.py
+++ hello_git.py
@@ -1,3 +1,9 @@
 print('Hello, Git!')
 print('Hello, Git!')
 print('Индексация - это важно')
+print('Create new branch')
+print('Hello, Git!')
+print('Indexing is important!')
+
+for num in range(10):
+        print(num,end=' ')
