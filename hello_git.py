2be312e71a85fc17306cd8e2f4de783407666cd5
Eric Gumerov <egumerov@gmail.com>
2025-08-15 15:58:38 +0800

    Update hello_git py. Add for

--- hello_git.py
+++ hello_git.py
@@ -7,3 +7,31 @@ print('Indexing is important!')
 
 for num in range(10):
         print(num,end=' ')
+        import os
+print('Hello, Git!')
+print('Indexing is important!')
+
+for num in range(10):
+        print(num,end=' ')
+print(f'\nYour current directory: {os.getcwd()}')
+print('Hello, Git!')
+print('Indexing is important!')
+
+ind = 0
+sum = 0
+
+while ind <= 9:
+        sum += ind
+        print(sum)
+        ind += 1
+import os
+print('Hello, Git!')
+print('Indexing is important!')
+
+for num in range(10):
+        print(num,end=' ')
+
+print(f'\nYour current directory: {os.getcwd()}')
+
+s = 'Version Control System'
+print(f'{s} - length is {len(s)} characters')
