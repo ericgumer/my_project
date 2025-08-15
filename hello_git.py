9ce71f3a1d29619d14ac2f56a47bdba4633f0fb1
Eric Gumerov <egumerov@gmail.com>
2025-08-15 16:26:02 +0800

    merch

--- hello_git.py
+++ hello_git.py
@@ -2,10 +2,8 @@ import os
 import b
 
 print('Hello, Git!')
-print('Hello, Git!')
-print('Индексация - это важно')
-print('Create new branch')
-print('Hello, Git!')
+
+
 print('Indexing is important!')
 
 
@@ -16,15 +14,7 @@ while ind <= 9:
         sum += ind
         print(sum)
         ind += 1
-import os
-print('Hello, Git!')
-print('Indexing is important!')
-
-for num in range(10):
-        print(num,end=' ')
-
-print(f'\nYour current directory: {os.getcwd()}')
 
 s = 'Version Control System'
 print(f'{s} - length is {len(s)} characters')
-print(b.sum_numbers(1,2,3,4,5))
+
